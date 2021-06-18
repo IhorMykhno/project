a978c5b3b835cfb3a2238a797fc2dc19139fb3c2
Ihor <mixnoiv@gmail.com>
2021-06-18 15:12:01 +0300

    Changing the date

--- exercise.py
+++ exercise.py
@@ -17,7 +17,6 @@ def fRow(file):
                 # changeMonth(tmp)
                 firstRow.append(tmp)
             # Розташування співробітників по алфавіту  
-            
             if not(row[0] in sortNames):
                 sortNames.append(row[0])   
 
@@ -33,28 +32,62 @@ def fRow(file):
     sortNames.sort()   
 
 def changeMonth(date):
-    # Початковий місяць
-    month = (date.split(sep = ' ', maxsplit = 1))[0]
+    newDate = []
+    newDate.append('Name/Date')
+    # print(date)
+    for i in range(len(date)):
+        month = date[i]
+        # Розбиття вхідної дати 
+        month = month.split(sep = ' ', maxsplit = 2)
+        # Генерування дати поданої в шаблоні
+        newMonth = f'{month[2]}-{searchMonth(month[0])}-{month[1]}'
+        newDate.append(newMonth)
+    # print(newDate)
+    return newDate   
+    # date[0] = searchMonth(month)
     # Заміна на числове позначення місяця
-    date = date.replace(month, searchMonth(month))
+    # date = date.replace(month, searchMonth(month))
 
 def searchMonth(month):
     allMonth = {
-        'Jan' : '1',
-        'Feb' : '2', 
-        'Mar' : '3',
-        'Apr' : '4',
-        'May' : '5',
-        'Jun' : '6',
-        'Jul' : '7',
-        'Aug' : '8', 
-        'Sept' : '9',
+        'Jan' : '01',
+        'Feb' : '02', 
+        'Mar' : '03',
+        'Apr' : '04',
+        'May' : '05',
+        'Jun' : '06',
+        'Jul' : '07',
+        'Aug' : '08', 
+        'Sept' : '09',
         'Oct' : '10',
         'Nov' : '11',
         'Dec' : '12'
     }
     return allMonth[month]
 
+def newStuffList():
+    global firstRow
+    global sortNames
+    global allStuff
+    global NewCSVdoc
+    # Лічильник сформованого списку
+    tmp = 0
+    for sName in sortNames:
+        NewCSVdoc.append([])
+        NewCSVdoc[tmp].append(sName)
+        # Заповнення в новому списку 0 рядка робочих часів
+        for j in range(1, len(firstRow)+1):
+            NewCSVdoc[tmp].append(0)
+
+        for i in range(len(allStuff)):
+            # Підстановка відповідного ім'я по алфавіту
+            if sName == allStuff[i][0]:
+                idx =  firstRow.index(allStuff[i][1]) 
+                # print(idx)
+                # print(allStuff[i][2])
+                NewCSVdoc[tmp][idx+1] = allStuff[i][2]  
+                # NewCSVdoc[tmp].append(allStuff[i][1])
+        tmp += 1            
 
 # Перший радок 
 firstRow = []
@@ -62,8 +95,11 @@ firstRow = []
 # Список з відсортованими   
 sortNames = []
 
-# 
+# Перелік даних про співробітників
 allStuff = []
+
+# Список з даними які будуть виводитись в новий CSV
+NewCSVdoc = []
 
 # Читання даних з CSV документа
 with open("C:/project/project/acme_worksheet.csv", encoding='utf-8') as r_file:    
@@ -71,7 +107,9 @@ with open("C:/project/project/acme_worksheet.csv", encoding='utf-8') as r_file:
     file_reader = csv.reader(r_file, delimiter = ",")
     fRow(file_reader)
 
-# print(firstRow)
-# print(sortNames)  
-allStuff.sort(key = lambda x: x[0][0])
-print(allStuff)
+newStuffList()
+NewRow = changeMonth(firstRow)
+
+print(NewRow)
+print(NewCSVdoc)
+# print(allStuff)
