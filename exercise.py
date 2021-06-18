9648f3bfda23a29fdffaa3980b98fb4bb9627ee7
Ihor <mixnoiv@gmail.com>
2021-06-18 17:08:32 +0300

    Cleaning the code

--- exercise.py
+++ exercise.py
@@ -7,11 +7,14 @@ def fRow(file):
     global firstRow
     global sortNames
     global allStuff
+
+    # Допоміжна зміння для формування першого рядка в CSV
     tmp = ''
     # Індекс списку
     iList = 0
     # Відкидання першого елемента
     firstElem = 1
+
     for row in file:
         if firstElem == 0:
             # Заповнення першого ярдка для виведення 
@@ -19,6 +22,7 @@ def fRow(file):
                 tmp = row[1]
                 # changeMonth(tmp)
                 firstRow.append(tmp)
+
             # Розташування співробітників по алфавіту  
             if not(row[0] in sortNames):
                 sortNames.append(row[0])   
@@ -29,28 +33,24 @@ def fRow(file):
             allStuff[iList].append(row[1])
             allStuff[iList].append(row[2])
             iList += 1
+
         else:
             firstElem = 0        
-
     sortNames.sort()   
 
 def changeMonth(date):
+    # Зберігаються відредаговані дані першого рядка
     newDate = []
     newDate.append('Name/Date')
-    # print(date)
+
     for i in range(len(date)):
         month = date[i]
         # Розбиття вхідної дати 
         month = month.split(sep = ' ', maxsplit = 2)
         # Генерування дати поданої в шаблоні
         newMonth = f'{month[2]}-{searchMonth(month[0])}-{month[1]}'
-        # newMonth = month[2] + searchMonth(month[0]) + month[1]
         newDate.append(newMonth)
-    # print(newDate)
     return newDate   
-    # date[0] = searchMonth(month)
-    # Заміна на числове позначення місяця
-    # date = date.replace(month, searchMonth(month))
 
 def searchMonth(month):
     allMonth = {
@@ -75,23 +75,20 @@ def newStuffList():
     global allStuff
     global NewCSVdoc
     # Лічильник сформованого списку
-    tmp = 0
+    counter = 0
     for sName in sortNames:
         NewCSVdoc.append([])
-        NewCSVdoc[tmp].append(sName)
+        NewCSVdoc[counter].append(sName)
         # Заповнення в новому списку 0 рядка робочих часів
         for j in range(1, len(firstRow)+1):
-            NewCSVdoc[tmp].append(0)
+            NewCSVdoc[counter].append(0)
 
         for i in range(len(allStuff)):
             # Підстановка відповідного ім'я по алфавіту
             if sName == allStuff[i][0]:
                 idx =  firstRow.index(allStuff[i][1]) 
-                # print(idx)
-                # print(allStuff[i][2])
-                NewCSVdoc[tmp][idx+1] = allStuff[i][2]  
-                # NewCSVdoc[tmp].append(allStuff[i][1])
-        tmp += 1            
+                NewCSVdoc[counter][idx+1] = allStuff[i][2]  
+        counter += 1            
 
 def srchCatalog():
     path = abspath(getsourcefile(lambda:0))
@@ -99,38 +96,34 @@ def srchCatalog():
     path = path + "\\acme_worksheet.csv"
     return path
 
-# Перший радок 
+# Перший радок для CSV документу
 firstRow = []
 
-# Список з відсортованими   
+# Список з відсортованими іменами  
 sortNames = []
 
-# Перелік даних про співробітників
+# Всі дані про співробітників
 allStuff = []
 
-# Список з даними які будуть виводитись в новий CSV
+# Список з даними, які будуть виводитись в новий CSV
 NewCSVdoc = []
 
+#Пошук шляху до CSV файла 
 linkOfCatalog = srchCatalog()
 
 # Читання даних з CSV документа
 with open(linkOfCatalog, encoding='utf-8') as r_file:   
-# with open("C:/project/project/acme_worksheet.csv", encoding='utf-8') as r_file:    
-    # Создаем объект reader, указываем символ-разделитель ","
     file_reader = csv.reader(r_file, delimiter = ",")
     fRow(file_reader)
 
+# Формування даних для виведення в новий CSV
 newStuffList()
-firstRow = changeMonth(firstRow)
 
-# print(firstRow)
-# print(NewCSVdoc)
+# Редагування першого рядка за шаблоном
+firstRow = changeMonth(firstRow)
 
 # Виведення 
 with open("C:/project/project/new_acme_worksheet.csv", 'w', newline="") as w_file:    
      writer = csv.writer(w_file)
      writer.writerows([firstRow])
      writer.writerows(NewCSVdoc)
-
-
-# print(path)
